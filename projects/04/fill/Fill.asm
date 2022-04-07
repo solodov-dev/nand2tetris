71410e6819a470a119b96d02faf69215ce66b5ab
Andrey Solodov <solodov.dev@gmail.com>
2022-04-07 12:00:29 +0300

    Fill screen program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Watch for keyboard
+(LOOP)
+  @KBD
+  D=M
+  @BLACK
+  D;JGT
+  @WHITE
+  0;JMP
+
+// Set color to black
+(BLACK)
+  @COLOR
+  M=-1
+  @FILL
+  0;JMP
+
+// Set color to white
+(WHITE)
+  @COLOR
+  M=0
+  @FILL
+  0;JMP
+
+(FILL)
+  // Set starting pixel index to the first pixel of screen
+  @SCREEN
+  D=A
+  @PIXEL_IDX
+  M=D
+  (DRAW)
+    // Get color
+    @COLOR
+    D=M
+    // Set RAM[PIXEL_IDX] to color
+    @PIXEL_IDX
+    A=M
+    M=D
+    // Increase pixel index
+    @PIXEL_IDX
+    MD=M+1
+    // Last pixel index is KBD memory address - 1
+    // Check that we are still in the screen RAM boundaries
+    @KBD
+    D=D-A
+    @LOOP
+    D;JEQ
+  @DRAW
+  0;JMP
+
+
+
+
