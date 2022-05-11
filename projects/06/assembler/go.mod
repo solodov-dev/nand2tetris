ef7649db7516b67e4e97b00cfef228552179dee0
Andrey Solodov <solodov.dev@gmail.com>
2022-05-11 16:08:07 +0300

    assembler parser package

---
+++ projects/06/assembler/go.mod
@@ -0,0 +1,3 @@
+module nand/parser
+
+go 1.18
---
+++ projects/06/assembler/parser.go
@@ -0,0 +1,45 @@
+// Package parser parses input file and reads it line by line
+package parser
+
+import (
+  "bufio"
+  "fmt"
+  )
+
+type CommandType int
+
+const (
+  A_COMMAND CommandType = 0
+  C_COMMAND
+  L_COMMAND
+  )
+
+var currentLine string
+
+func hasMoreComands() bool {
+   
+}
+
+func advance() {
+
+}
+
+func commandType() CommandType {
+
+}
+
+func symbol() string {
+
+}
+
+func dest() string {
+
+}
+
+func comp() string {
+
+}
+
+func jump() string {
+
+}
