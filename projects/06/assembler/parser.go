a64b7e398f42505198c91f81f3695326d9420573
Andrey Solodov <solodov.dev@gmail.com>
2022-05-23 12:09:07 +0300

    assembler parser and code modules

--- projects/06/assembler/parser.go
+++ projects/06/assembler/parser.go
@@ -1,45 +1,108 @@
 // Package parser parses input file and reads it line by line
-package parser
+package assembler
 
 import (
-  "bufio"
-  "fmt"
-  )
+	"bufio"
+	"strings"
+)
 
-type CommandType int
+type Command int
 
 const (
-  A_COMMAND CommandType = 0
-  C_COMMAND
-  L_COMMAND
-  )
+	A_COMMAND Command = 0
+	C_COMMAND Command = 1
+	L_COMMAND Command = 2
+)
 
-var currentLine string
-
-func hasMoreComands() bool {
-   
+type Parser struct {
+	scanner        *bufio.Scanner
+	currentCommand string
 }
 
-func advance() {
-
+type ParserInterface interface {
+	Advance()
+	CommandType() Command
+	Symbol() string
+	Dest() string
+	Comp() string
+	Jump() string
 }
 
-func commandType() CommandType {
-
+func New(scanner *bufio.Scanner) *Parser {
+	return &Parser{scanner, ""}
 }
 
-func symbol() string {
+func (p *Parser) HasMoreComands() bool {
+	hasCommands := p.scanner.Scan()
 
+	if !hasCommands {
+		return false
+	}
+
+	if ignoreNextLine(p) {
+		return p.HasMoreComands()
+	}
+
+	return true
 }
 
-func dest() string {
-
+func (p *Parser) Advance() {
+  p.currentCommand = strings.TrimSpace(p.scanner.Text())
 }
 
-func comp() string {
+func (p *Parser) CommandType() Command {
+	if strings.HasPrefix(p.currentCommand, "@") {
+		return A_COMMAND
+	}
 
+	if strings.HasPrefix(p.currentCommand, "(") {
+		return L_COMMAND
+	}
+
+	return C_COMMAND
 }
 
-func jump() string {
+func (p *Parser) Symbol() string {
+	if p.CommandType() == A_COMMAND {
+		return strings.TrimPrefix(p.currentCommand, "@")
+	} else {
+		return strings.Trim(p.currentCommand, "()")
+	}
+}
 
+func (p *Parser) Dest() string {
+	index := strings.IndexByte(p.currentCommand, '=')
+	if index < 0 {
+		return ""
+	}
+	return p.currentCommand[:index]
+}
+
+func (p *Parser) Comp() string {
+	startIndex := strings.IndexByte(p.currentCommand, '=')
+  if startIndex < 0 {
+    startIndex = 0
+  }
+
+	endIndex := strings.IndexByte(p.currentCommand, ';')
+  if (endIndex < 0) {
+    return p.currentCommand[startIndex:]
+  }
+  return p.currentCommand[startIndex:endIndex]
+}
+
+func (p *Parser) Jump() string {
+	index := strings.IndexByte(p.currentCommand, ';')
+	if index < 0 {
+		return ""
+	}
+	return p.currentCommand[index:]
+}
+
+func ignoreNextLine(p *Parser) bool {
+	nextLine := p.scanner.Text()
+	if nextLine == "" || strings.HasPrefix(nextLine, "//") {
+		return true
+	}
+	return false
 }
